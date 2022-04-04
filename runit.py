51aae651e5e62fac944e921da400f9b896f0a20d
gauravbaluni <baluni.gaurav@gmail.com>
2022-04-04 17:03:23 +0530

    Delete runit.py

--- runit.py
+++
@@ -1,9 +0,0 @@
-import urllib, json
-
-URL = "https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByPin?pincode=400094&date=01-05-2021"
-
-response = urllib.urlopen(url)
-data = json.loads(response.read())
-print data
-
-
